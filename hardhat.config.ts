00b1bcaec2fe5a709fdfce5a356b259162ee419d
kyrielam <lamkencong@gmail.com>
2022-11-07 18:18:34 +0800

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,48 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import 'solidity-coverage';
+import 'hardhat-gas-reporter';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+export default {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    localhost: {
+      allowUnlimitedContractSize: true,
+      url: `http://127.0.0.1:8545`,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: false,
+    excludeContracts: [],
+    src: './contracts',
+  },
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        bytecodeHash: 'none',
+      },
+    },
+  },
 };
-
-export default config;
